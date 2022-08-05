e40138a994c5ac727c6fe40795ddd85b2f3cfdd2
sujemuje <mateusz.sujewicz@op.pl>
2022-08-05 21:41:56 +0200

    ale kurwa zajebisty kalkulatoreeek

--- src/calculator_command.py
+++ src/calculator_command.py
@@ -2,31 +2,52 @@ import discord
 
 
 result: float = 0
-operation: str = ''
-recent: int = 0
+text_buffer: [str] = ['0', '']
+operation: chr = '+'
+recent: int = -1
 
 
 def init(tree):
+    def compute(oper: chr):
+        global text_buffer, operation, result
+        if operation == '+':
+            result = int(text_buffer[0]) + int(text_buffer[1])
+        elif operation == '-':
+            result = int(text_buffer[0]) - int(text_buffer[1])
+        elif operation == '*':
+            result = int(text_buffer[0]) * int(text_buffer[1])
+        elif operation == '/':
+            result = int(text_buffer[0]) / int(text_buffer[1])
+        text_buffer[0] = str(result)
+        text_buffer[1] = ''
+        operation = oper
+
     class CalculatorButton(discord.ui.Button):
         def __init__(self, value, *args, **kwargs):
             self.value: int = value
+            global recent
+            self.index = recent + 1
+            recent += 1
             super().__init__(*args, **kwargs)
 
         async def callback(self, interaction: discord.Interaction):
             global recent
             self.view.children[recent].style = discord.ButtonStyle.gray
-            recent = self.value - 1
-
+            recent = self.index
             """Inna metoda
             for child in self.view.children:
                 if child.style == discord.ButtonStyle.green:
                     child.style = discord.ButtonStyle.gray
                     break
             """
-
             self.style = discord.ButtonStyle.green
-            global result
-            result = self.value
+
+            global text_buffer, result
+            if self.value == -1:
+                compute(self.label)
+            else:
+                text_buffer[1] += self.label
+                result = text_buffer[1]
             await interaction.response.edit_message(content=result, view=self.view)
 
     class CalculatorView(discord.ui.View):
@@ -36,11 +57,29 @@ def init(tree):
 
         def __init__(self):
             super().__init__()
-            for y in range(3):
-                for x in range(3):
+            for y in range(4):
+                for x in range(4):
+                    if x < 3 and y < 3:
+                        v = 6 - (3 * y) + x + 1
+                        l = str(v)
+                    else:
+                        v = -1
+                        l = ' '
+                        if x == 1 and y == 3:
+                            v = 0
+                            l = '0'
+                        elif x == 2 and y == 3:
+                            l = '.'
+                        elif x == 3:
+                            if y == 0:    l = '+'
+                            elif y == 1:  l = '-'
+                            elif y == 2:  l = '*'
+                            elif y == 3:  l = '/'
+                        else:
+                            v = -2
                     self.add_item(CalculatorButton(
-                        value=(3 * y + x + 1),
-                        label=str(3 * y + x + 1),
+                        value=v,
+                        label=l,
                         row=y,
                         style=discord.ButtonStyle.gray)
                     )
