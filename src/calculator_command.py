b8a80db85f4f04d173eb62ec56d46cbbd8c86b4d
sujemuje <mateusz.sujewicz@op.pl>
2022-08-06 19:09:25 +0200

    added descriptions to calculator_command.py

--- src/calculator_command.py
+++ src/calculator_command.py
@@ -6,54 +6,73 @@ import calculator
 def init(tree):
 
     class CalculatorButton(discord.ui.Button):
-        def __init__(self, value, calc, *args, **kwargs):
-            self.value: int = value
+        """
+        Reprezentacja przycisku kalkulatora za pomocą discord.ui.Button
+        """
+        def __init__(self, calc, *args, **kwargs):
+            """
+            Inicjalizacja klasy
+            :param calc: obiekt klasy Calculator; odpowiada za obliczeniową stronę całej komendy
+            """
             super().__init__(*args, **kwargs)
             self.calc = calc
 
-        async def callback(self, interaction: discord.Interaction):
-            # interaction.client.calculators[interaction.original_message().fetch().id]
+        async def callback(self, interaction: discord.Interaction) -> None:
+            """
+            Reakcja na naciśnięcie przycisku
+            :param interaction: discord.Interaction
+            :return: None
+            """
+            # Aktualizacja koloru przycisków
             for child in self.view.children:
                 if child.style == discord.ButtonStyle.green:
                     child.style = discord.ButtonStyle.gray
                     break
             self.style = discord.ButtonStyle.green
 
+            # Przekazanie danych o naciśniętym przycisku; odpowiednimi reakcjami zarządza metoda klasy Calculator
             self.calc.execute(self.label)
+            # Aktualizacja komendy
             await interaction.response.edit_message(content=self.calc.get_result(), view=self.view)
 
     class CalculatorView(discord.ui.View):
-        """Reprezentacja kalkulatora za pomocą przycisków pod wiadomością, WIP"""
-
+        """
+        Reprezentacja kalkulatora za pomocą discord.ui.View zawierającego przyciski - CalculatorButton
+        """
         def __init__(self, calc):
+            """
+            Inicjalizacja klasy
+            :param calc: obiekt klasy Calculator;
+            będzie przekazywany przyciskom, by te mogły wywoływać na nim odpowiednią metodę przy callbacku
+            """
             super().__init__()
             self.calc = calc
 
+            # Tworzenie przycisków z odpowiednimi podpisami
             for y in range(4):
                 for x in range(4):
                     if x < 3 and y < 3:
-                        v = 6 - (3 * y) + x + 1
-                        l = str(v)
+                        label = str(6 - (3 * y) + x + 1)
                     else:
-                        v = -1
-                        l = ' '
+                        label = ' '
                         if x == 1 and y == 3:
-                            v = 0
-                            l = '0'
+                            label = '0'
                         elif x == 2 and y == 3:
-                            l = '.'
+                            label = '.'
                         elif x == 3:
-                            if y == 0:    l = '+'
-                            elif y == 1:  l = '-'
-                            elif y == 2:  l = '*'
-                            elif y == 3:  l = '/'
+                            if y == 0:
+                                label = '+'
+                            elif y == 1:
+                                label = '-'
+                            elif y == 2:
+                                label = '*'
+                            elif y == 3:
+                                label = '/'
                         else:
-                            v = -2
-                            l = 'C'
+                            label = 'C'
                     self.add_item(CalculatorButton(
-                        value=v,
                         calc=self.calc,
-                        label=l,
+                        label=label,
                         row=y,
                         style=discord.ButtonStyle.gray)
                     )
@@ -65,9 +84,6 @@ def init(tree):
     async def __command_kalkulator(
             interaction: discord.Interaction
     ) -> None:
-        x: calculator.Calculator = calculator.Calculator()  # interaction)
-        view = CalculatorView(x)
+        calc: calculator.Calculator = calculator.Calculator()
+        view = CalculatorView(calc)
         await interaction.response.send_message(content='0', view=view)
-        #client: custom_client.Client = interaction.client
-
-        #client.calculators[interaction.original_message().fetch().id] = calculator.Calculator(interaction)
--- src/custom_client.py
+++ src/custom_client.py
@@ -1,9 +1,6 @@
 import discord
-import calculator
-from typing import *
 
 
 class Client(discord.Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.calculators: Dict[int, calculator.Calculator] = {}
