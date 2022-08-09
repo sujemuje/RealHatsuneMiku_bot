8cc6ab09c46b33469016e780e01b461298952f00
sujemuje <mateusz.sujewicz@op.pl>
2022-08-10 00:56:38 +0200

    finished /poll command
    (jako tako)

--- src/poll_command.py
+++ src/poll_command.py
@@ -1,3 +1,4 @@
+import asyncio
 import discord
 from typing import *
 
@@ -44,6 +45,7 @@ class OptionButton(discord.ui.Button):
 class VotersButton(discord.ui.Button):
     def __init__(self, opt_count):
         super().__init__(label='List of voters', emoji='📜', style=discord.ButtonStyle.primary)
+        self.votes = 0
         self.voters = [[] for i in range(opt_count)]
 
     async def callback(self, interaction: discord.Interaction):
@@ -59,15 +61,58 @@ class VotersButton(discord.ui.Button):
         await interaction.response.send_message(content=content, ephemeral=True)
 
 
+def convert_time(t: str):
+    if t is None:
+        return None
+    sorted_time = 0
+    i = -1
+    for x in t:
+        i += 1
+        if not x.isnumeric():
+            if x == 'd':
+                sorted_time += int(t[:i]) * 24 * 60 * 60
+            elif x == 'h':
+                sorted_time += int(t[:i]) * 60 * 60
+            elif x == 'm':
+                sorted_time += int(t[:i]) * 60
+            elif x == 's':
+                sorted_time += int(t[:i])
+            else:
+                return -1
+            if len(t) > i:
+                t = t[i+1:]
+            i = -1
+
+    return sorted_time
+
+
 class PollView(discord.ui.View):
-    def __init__(self, opt_count: int, public: bool):
+    def __init__(self, opt_count: int, public: bool, duration: int, interaction: discord.Interaction):
+        self.duration = duration
         super().__init__(timeout=None)
         self.voters = []
+        self.interaction = interaction
 
         for opt in range(opt_count):
             self.add_item(OptionButton(order=opt, public=public))
         if public:
             self.add_item(VotersButton(opt_count=opt_count))
+
+    async def on_timeout(self) -> None:
+        print("TIMEOUT")
+        m = 0
+        option = ''
+        s = 0
+        for child in self.children:
+            s += child.votes
+            if child.votes > m:
+                m = child.votes
+                option = child.emoji
+            child.disabled = True
+
+        await self.interaction.followup.send(
+            f'Wygrała opcja nr {option} z ilością głosów {m} ({100* (m / s)}%)!'
+        )
 
 
 def init(tree):
@@ -78,6 +123,7 @@ def init(tree):
     async def __command_poll(
             interaction: discord.Interaction,
             public: bool,
+            duration: Optional[str | None],
             title: str,
             opt1: str,
             opt2: str,
@@ -93,6 +139,7 @@ def init(tree):
         # modal = MyModal(title=title, options=options)
         # await interaction.response.send_modal(modal)
 
+        duration = convert_time(duration)
         options = [opt1, opt2, opt3, opt4, opt5, opt6, opt7, opt8, opt9, opt10]
         content = f'{title}:'
         i = 0
@@ -101,5 +148,9 @@ def init(tree):
                 content += f'\n{emotes[i]} - {option}'
                 i += 1
 
-        view = PollView(opt_count=i, public=public)
+        view = PollView(opt_count=i, public=public, duration=duration, interaction=interaction)
         await interaction.response.send_message(content=content, view=view)
+        if duration:
+            await asyncio.sleep(duration)
+            view.stop()
+            await view.on_timeout()
