e46701f02bb79839eb285a6e8673b976aade4a35
sujemuje <mateusz.sujewicz@op.pl>
2022-08-14 01:45:41 +0200

    image catgirl - embeded

--- src/image_command.py
+++ src/image_command.py
@@ -16,12 +16,32 @@ def init(tree):
             async with session.get(f'https://api.rule34.xxx/index.php?page=dapi&pid={random.randint(1, 20)}&s=post&q=index&tags={tag}&json=1') as resp:
                 posts: list = await resp.json()
                 posts.sort(key=lambda i: i['score'], reverse=True)
-                message = f"Randomly searched for tag **{tag.capitalize()}**. Here are few best results: \n"
+                i, l = 0, 10 if len(posts) > 10 else len(posts)
+                while i < l:
+                    if posts[i]['file_url'][-3:] == 'mp4':
+                        posts.pop(i)
+                    else:
+                        i += 1
+
+                link = posts[random.randint(0, l - 1)]['file_url']
+
+                embed = discord.Embed.from_dict(
+                    {
+                        "title": tag,
+                        "type": "image",
+                        "image": {
+                            "url": link
+                        },
+                        "color": 0xff6666
+                    }
+                )
+                await interaction.response.send_message(embed=embed)
+
+                """message = f"Randomly searched for tag **{tag.capitalize()}**. Here are few best results: \n"
                 i = 0
                 for post in posts:
                     message += f'\timage link = {post["file_url"]}, score = {post["score"]}\n'
                     i += 1
                     if i == 7:
                         break
-                await interaction.response.send_message(message)
-
+                await interaction.response.send_message(message)"""
